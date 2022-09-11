4646fc2ba5b39f549ae25fca99af49ea5e95d55f
Steve <steveed0190@gmail.com>
2022-09-10 23:36:30 -0400

    Week1-Practice

---
+++ src/com/steve/Week1.java
@@ -0,0 +1,20 @@
+package com.steve;
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		
+		String carBrand ="Nissan Rogue";
+		
+		int carPrice = 12000;
+		
+		char trimLevel = 'S';
+		
+		int carYear = 2018;
+		
+		
+		System.out.println("The " + carYear + " " + carBrand + " costs $" + carPrice + ".");
+
+	}
+
+}
